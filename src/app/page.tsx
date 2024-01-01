8c3d6291d476372f399e3ab0e49d631823864f8d
zinny22 <hziny722@naver.com>
2024-01-01 20:24:33 +0900

    feat : next & tailwind 사용 구조로 변경

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+/** @format */
+
+import Header from "@/Components/Header";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
