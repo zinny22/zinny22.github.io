39d119b88b78f5d7cf3682da348ced29ef521cee
zinny22 <hziny722@naver.com>
2024-01-16 20:57:50 +0900

    fix : css 경로 수정

--- next.config.js
+++ next.config.js
@@ -4,13 +4,12 @@
  */
 
 const debug = process.env.NODE_ENV !== "production";
-const repository = "zinny-portfolio";
 
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
-  assetPrefix: !debug ? `/${repository}/` : "", // production 일때 prefix 경로
-  trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
+  assetPrefix: !debug ? `/` : "",
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
